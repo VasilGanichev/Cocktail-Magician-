6913601a1b97b9df83ff8f34acbaf4f7279e771d
VasilGanichev <vasilganichev01@gmail.com>
2019-11-01 11:43:23 +0200

    Added configurations for bars, user and reviews

---
+++ Final project/CocktailMagicianWeb/CocktailMagician.Data/Configurations/BarConfig.cs
@@ -0,0 +1,33 @@
+﻿using CocktailMagician.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocktailMagician.Data.Configurations
+{
+    public class BarConfig : IEntityTypeConfiguration<Bar>
+    {
+        public void Configure(EntityTypeBuilder<Bar> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .IsRequired();
+
+            builder.Property(b => b.Address)
+                .IsRequired();
+
+            builder.Property(b => b.PhoneNumber)
+                .IsRequired();
+
+            builder.Property(b => b.Picture)
+                .IsRequired();
+
+            builder.HasMany(b => b.Reviews)
+                .WithOne(r => r.Bar)
+                .HasForeignKey(r => r.BarId);
+        }
+    }
+}
--- Final project/CocktailMagicianWeb/CocktailMagician.Data/Entities/User.cs
+++ Final project/CocktailMagicianWeb/CocktailMagician.Data/Entities/User.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CocktailMagician.Data.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
+        public bool IsBanned { get; set; }
+        public int ReviewId { get; set; }
+        public List<Review>Reviews { get; set; }
     }
 }
