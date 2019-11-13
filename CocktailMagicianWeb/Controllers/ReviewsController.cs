ffb477bc903857845e633224440393a5208c6cec
VasilGanichev <vasilganichev01@gmail.com>
2019-11-13 18:01:50 +0200

    Fixed bug with missing photo

--- CocktailMagicianWeb/Controllers/ReviewsController.cs
+++ CocktailMagicianWeb/Controllers/ReviewsController.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CocktailMagician.Data.Entities;
+using CocktailMagician.Services.Contracts;
 using CocktailMagicianWeb.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CocktailMagicianWeb.Controllers
 {
     public class ReviewsController : Controller
     {
+        private readonly IBarServices _barServices;
+        private readonly IReviewServices _reviewServices;
+        private readonly UserManager<User> _userManager;
+
+        public ReviewsController(IBarServices barServices, IReviewServices reviewServices, UserManager<User> userManager)
+        {
+            _barServices = barServices;
+            _reviewServices = reviewServices;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,13 +30,20 @@ namespace CocktailMagicianWeb.Controllers
         [HttpGet]
         public async Task<IActionResult> LeaveBarReview(int Id)
         {
-            return View();
+            var viewModel = new ReviewViewModel
+            {
+                Id = Id,
+            };
+            return View(viewModel);
         }
 
         [HttpPost]
         public async Task<IActionResult> LeaveBarReview(ReviewViewModel viewModel)
         {
-            return View();
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var bar = await _barServices.GetBarAsync(viewModel.Id);
+            await _reviewServices.CreateReviewAsync(viewModel.Rating, viewModel.Comment, bar, null, user);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
